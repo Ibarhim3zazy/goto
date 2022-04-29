eb559fba49d4f088dc02c46fbb9a281d5ca79d7b
Ibrahim 3zazy <83598736+Ibarhim3zazy@users.noreply.github.com>
2022-04-29 09:09:44 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 function Goto() {
     window.location.href = 'https://kindness.lovestoblog.com/';
 }
+function Goto_comparison() {
+    window.location.href = 'https://kindness.lovestoblog.com/comparison.php';
+}
+function Goto_meContact() {
+    window.location.href = 'https://kindness.lovestoblog.com/Ibrahim_contact.php';
+}
+function Goto_mohamed() {
+    window.location.href = 'https://kindness.lovestoblog.com/Mohamed_contact.php';
+}
+function Goto_mostafa() {
+    window.location.href = 'https://kindness.lovestoblog.com/Mostafa_contact.php';
+}
